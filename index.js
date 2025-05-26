337f4062a633c90b182ba4b84f3dc83c1ac2d3a9
Lucas emanoel <lucasemanoel625@gmail.com>
2025-05-26 20:59:47 -0300

    feat: instalação do bootstrap, estilização com bootstrap e envio de formulário para o node

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/:nome/:lang', (req, res) => {
-  let nome = req.params.nome;
-  let lang = req.params.lang;
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "Itau"
-  })
+app.get('/', (req, res) => {
+  res.render("index")
 });
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar")
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  res.send("Pergunta recebida com sucesso!");
+})
+ 
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
